333d9001d6c1ef7f71a2bcdfe0e21915434db3f2
Niole Nelson <niolenelson@gmail.com>
2016-11-13 15:54:19 -0800

    adds new test files and processed dir

---
+++ test2.js
@@ -0,0 +1,29 @@
+var kl = require('_i-dfm/jj0000');
+var losdf = require("aaaaa");
+import pop from 'nioleimport';
+
+for var i=sdflaskdjfasdf
+  alsdkfjasdf
+sdlfkjdsflkdsjfldskfj'
+
+var j = require('asdfKat');
+var p = require('nioleimport');
+import i from './idfm';
+
+var x = 2 + 4;
+
+function F(hello, goodbye) {
+  var d = "hello";
+  d = hello;
+  return goodbye;
+}
+
+var i = require('./idfm');
+import k from '_i-dfm';
+import z from './../ssdf/ff/mmm';
+import losdf from "aaaaa";
+import j from 'asdfKat';
+var k = require('_i-dfm');
+var z = require('./../ssdf/ff/mmm');
+import kl from '_i-dfm/jj0000';
+
